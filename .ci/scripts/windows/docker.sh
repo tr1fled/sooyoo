f4949ecb42f82f4a1cac8b1d1c82c56d245d4eb3
niansa <tuxifan@posteo.de>
2024-03-13 03:10:14 +0100

    Add initial Windows CI

--- .ci/scripts/windows/docker.sh
+++ .ci/scripts/windows/docker.sh
@@ -7,47 +7,34 @@ set -e
 
 #cd /suyu
 
-ccache -sv
+wine ccache.exe -sv
 
 mkdir -p build && cd build
-cmake .. \
+wine cmake.exe .. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE="${PWD}/../CMakeModules/MinGWCross.cmake" \
+    -DCMAKE_INSTALL_PREFIX=C:/windows-libs/mingw64/ \
     -DDISPLAY_VERSION="$1" \
-    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
-    -DENABLE_QT_TRANSLATION=ON \
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF \
+    -DENABLE_QT_TRANSLATION=OFF \
     -DUSE_CCACHE=ON \
     -DSUYU_USE_BUNDLED_SDL2=OFF \
     -DSUYU_USE_EXTERNAL_SDL2=OFF \
+    -DENABLE_LIBUSB=NO \
     -DSUYU_TESTS=OFF \
     -GNinja
-ninja suyu suyu-cmd
+wine ninja.exe suyu suyu-cmd
 
-ccache -sv
+wine ccache.exe -sv
 
 echo "Tests skipped"
+# TODO: actually run the tests
 #ctest -VV -C Release
 
 echo 'Prepare binaries...'
 cd ..
 mkdir package
 
-if [ -d "/usr/x86_64-w64-mingw32/lib/qt5/plugins/platforms/" ]; then
-  QT_PLUGINS_PATH='/usr/x86_64-w64-mingw32/lib/qt5/plugins'
-else
-  #fallback to qt
-  QT_PLUGINS_PATH='/usr/x86_64-w64-mingw32/lib/qt/plugins'
-fi
-
 find build/ -name "suyu*.exe" -exec cp {} 'package' \;
-
-# copy Qt plugins
-mkdir package/platforms
-cp -v "${QT_PLUGINS_PATH}/platforms/qwindows.dll" package/platforms/
-cp -rv "${QT_PLUGINS_PATH}/mediaservice/" package/
-cp -rv "${QT_PLUGINS_PATH}/imageformats/" package/
-cp -rv "${QT_PLUGINS_PATH}/styles/" package/
-rm -f package/mediaservice/*d.dll
 
 for i in package/*.exe; do
   # we need to process pdb here, however, cv2pdb
@@ -55,7 +42,7 @@ for i in package/*.exe; do
   x86_64-w64-mingw32-strip "${i}"
 done
 
-python3 .ci/scripts/windows/scan_dll.py package/*.exe package/imageformats/*.dll "package/"
+python3 .ci/scripts/windows/scan_dll.py package/*.exe "package/"
 
 # copy FFmpeg libraries
 EXTERNALS_PATH="$(pwd)/build/externals"
