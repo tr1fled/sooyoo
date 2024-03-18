46761981655aa5ee7a94be05ef5f4a29584d4163
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-03-17 22:25:31 -0600

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,4 +1,4 @@
-pacman -Syu git make mingw-w64-x86_64-SDL2 mingw-w64-x86_64-cmake mingw-w64-x86_64-python-pip mingw-w64-x86_64-qt5 mingw-w64-x86_64-toolchain autoconf libtool automake-wrapper zip --noconfirm
+pacman -Syu git make mingw-w64-x86_64-SDL2 mingw-w64-x86_64-cmake mingw-w64-x86_64-python-pip mingw-w64-x86_64-qt5 mingw-w64-x86_64-toolchain autoconf libtool automake-wrapper mingw-w64-x86_64-github-cli zip --noconfirm
 git submodule update --init --recursive
 echo 'PATH=/mingw64/bin:$PATH' >> ~/.bashrc
 echo 'PATH=$(readlink -e /c/VulkanSDK/*/Bin/):$PATH' >> ~/.bashrc
@@ -7,3 +7,4 @@ mkdir build && cd build
 cmake -G "MSYS Makefiles" -DSUYU_USE_BUNDLED_VCPKG=ON -DSUYU_TESTS=OFF ..
 make -j$(nproc) 
 zip -r suyu-build.zip ./build
+gh release upload windows suyu-build.zip
