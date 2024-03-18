51aed7f10fa9a262324d16834a83b097fbf3b08a
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-03-17 19:05:14 -0600

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -2,6 +2,6 @@ pacman -Syu git make mingw-w64-x86_64-SDL2 mingw-w64-x86_64-cmake mingw-w64-x86_
 echo 'PATH=/mingw64/bin:$PATH' >> ~/.bashrc
 echo 'PATH=$(readlink -e /c/VulkanSDK/*/Bin/):$PATH' >> ~/.bashrc
 cd sooyoo
-mkdir build && cd build
+mkdir build
 cmake -G "MSYS Makefiles" -DSUYU_USE_BUNDLED_VCPKG=ON -DSUYU_TESTS=OFF ..
 make -j$(nproc)
