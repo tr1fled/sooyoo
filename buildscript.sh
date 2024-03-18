c95f97dfe0b37102a5c8d2123175bba9fbe254a3
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-03-17 19:39:33 -0600

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,4 +1,5 @@
 pacman -Syu git make mingw-w64-x86_64-SDL2 mingw-w64-x86_64-cmake mingw-w64-x86_64-python-pip mingw-w64-x86_64-qt5 mingw-w64-x86_64-toolchain autoconf libtool automake-wrapper --noconfirm
+git submodule update --init --recursive
 echo 'PATH=/mingw64/bin:$PATH' >> ~/.bashrc
 echo 'PATH=$(readlink -e /c/VulkanSDK/*/Bin/):$PATH' >> ~/.bashrc
 cd sooyoo
